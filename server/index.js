e4da114101430e0ff85a0fd0cd027809395c2a3c
Maceina <maceinaaudrius@gmail.com>
2020-12-28 14:04:26 +0200

    commit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
-
+//1
 require("dotenv").config();
 
 const app = express();
